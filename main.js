2e68ed9aa203849eb98b620d653e23c33c208c2f
“Jerson <cardenas.suit@gmail.com>
2023-06-20 12:48:22 -0500

    Agregamos el carrito de compras con JS

--- main.js
+++ main.js
@@ -16,5 +16,28 @@ const mobileMenu = document.querySelector('.mobile-menu');
 menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    const isCarritoMenuClose = carritoMenu.classList.contains('inactive');
+
+    if (!isCarritoMenuClose) {
+        carritoMenu.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+// Para hacer click abrir y cerrar una ventana flotante del CARRITO DE COMPRAS
+const carritoIcon = document.querySelector('.navbar-shopping-cart');
+const carritoMenu = document.querySelector('.product-detail');
+
+carritoIcon.addEventListener('click', toggleCarritoMenu);
+
+// Condicion para cerrar el otro menu Mobile
+function toggleCarritoMenu(){
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    // Si damos click al menu carrito vamos a cerrar el menu mobileMenu
+    if (!isMobileMenuClose) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    carritoMenu.classList.toggle('inactive');
 }
